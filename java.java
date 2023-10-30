4eeddd20ff1a5ac9e045bd69c0740c7605b224c5
Shiv Prakash Singh <144921828+shivprakashsinghjii@users.noreply.github.com>
2023-10-30 19:27:29 +0530

    Create java.java

---
+++ java.java
@@ -0,0 +1,65 @@
+class Node {
+    int data;
+    Node next;
+
+    Node(int data) {
+        this.data = data;
+    }
+}
+
+class LinkedList {
+    Node head;
+
+    public void append(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node current = head;
+        while (current.next != null) {
+            current = current.next;
+        }
+        current.next = newNode;
+    }
+
+    public void display() {
+        Node current = head;
+        while (current != null) {
+            System.out.print(current.data + " -> ");
+            current = current.next;
+        }
+        System.out.println("null");
+    }
+
+    public void reverse() {
+        Node prev = null;
+        Node current = head;
+        Node next;
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+    }
+}
+
+public class Main {
+    public static void main(String[] args) {
+        LinkedList list = new LinkedList();
+        list.append(1);
+        list.append(2);
+        list.append(3);
+        list.append(4);
+
+        System.out.println("Original Linked List:");
+        list.display();
+
+        list.reverse();
+
+        System.out.println("Reversed Linked List:");
+        list.display();
+    }
+}
